461127e78ee2c55eafe9ba5270db869acf583c0d
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-05 03:27:42 +0000

    Checkpoint 21 - complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,20 +1,25 @@
-require_relative "entry.rb"
+ require_relative "entry.rb"
 
 class AddressBook
-    attr_accessor :entries
-  
+  attr_accessor :entries
+
   def initialize
-      @entries = []
+    @entries = []
   end
-  
+
   def add_entry(name, phone, email)
-      index = 0
-      @entries.each do |entry|
-          if name < entry.name
-             break
-          end
-          index += 1
+    index = 0
+    @entries.each do |entry|
+      if name == entry.name && phone == entry.phone && email == entry.email
+        break
       end
-      @entries.insert(index, Entry.new(name, phone, email))
+      index += 1
+  end
+    
+    @entries << Entry.new(name, phone, email)
+  end
+
+  def remove_entry(entry)
+    @entries.delete(entry)
   end
 end
--- models/entry.rb
+++ models/entry.rb
@@ -2,7 +2,7 @@ class Entry
   attr_accessor :name, :phone_number, :email
  def initialize(name, phone_number, email)
    @name = name
-   @phoe_number = phone_number
+   @phone_number = phone_number
    @email = email
  end
  def to_s
