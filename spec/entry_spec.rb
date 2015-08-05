00c1070c0045d2dd086f672054c6d05ff77a5400
Chris Steeb <chris.steeb.001@gmail.com>
2015-08-05 23:40:43 +0000

    Assignment 23 complete

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -20,11 +20,10 @@ RSpec.describe Entry do
     end
   end
   
-  context "#to_s" do
+  context "#.to_s" do
     it "prints an entry as a string" do
-      entry = Entry.new ('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expected_string = "Name: Ada Lovelave\nPhone Number; 010.012.1815\nEmail: augusta.king@lovelace.com"
-
       expect(entry.to_s).to eq(expected_string)
     end
   end
